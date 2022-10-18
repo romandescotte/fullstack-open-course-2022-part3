02b52a1ea91fdc233485f3c8c23e72f902c2ac13
Roman <romandescotte@gmail.com>
2022-10-18 13:14:29 +1300

    adds frontend

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use(express.static('build'));
 
 morgan.token('body', function (req, res) { return `Body request: ${JSON.stringify(req.body)}` })
 
